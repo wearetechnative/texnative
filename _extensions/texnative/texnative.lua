cb791ba14c4051dab8f0bc60e72777faa6485a53
Pim Snel <post@pimsnel.com>
2025-01-28 21:15:53 +0100

    cleanups, fontawsome

--- _extensions/texnative/texnative.lua
+++ _extensions/texnative/texnative.lua
@@ -1,5 +1,5 @@
 
--- filters/date-format.lua
+-- FILTERS/DATE-FORMAT.LUA
 function Meta(meta)
   if meta.date then
     local format = "(%d+)-(%d+)-(%d+)"
@@ -41,61 +41,18 @@ local function get_rows_data(rows, cell_color, strong)
         data = data .. ' & '
       end
 
-      -- change % into \% hack
+      -- CHANGE % INTO \% HACK
       data = data:gsub('([^\\])%%', '%1\\%%')
       data = data:gsub('^%%', '\\%%')
     end
-    data = data .. '\n \\hline \n'
+    data = data ..'\n \\hline \n'
 
   end
   return data
 end
 
--- \begin{tabular}{| >{\raggedleft\arraybackslash}p{1.7cm} | >{\raggedright\arraybackslash}p{4.7cm} | >{\raggedright\arraybackslash}p{2.7cm}  | >{\raggedright\arraybackslash}p{2.7cm} | }
---
---   \hline
---
---   \cellcolor{tableheadercolor} \bf{Priority} &
---   \cellcolor{tableheadercolor} \bf{Response time (within service hours)} &
---   \cellcolor{tableheadercolor} \bf{Incident type} &
---   \cellcolor{tableheadercolor} \bf{Standard}
---   \\
---   \hline
---
---   \cellcolor{tableheadercolor} {\bf Critical}
---   & Within 30 minutes
---   & Entire infrastructure not accessible, security incident.
---   & 80\% \\ [0.2cm]
---   \hline
---
---   \cellcolor{tableheadercolor} {\bf High}
---   & Within 4 hours
---   & Part of the infrastructure is not accessible
---   & 80\% \\ [0.2cm]
---   \hline
---
---   \cellcolor{tableheadercolor} {\bf Medium}
---   & Withing 8 hours
---   & Account creation, changes etc..
---   & 80\% \\ [0.2cm]
---   \hline
---
---   \cellcolor{tableheadercolor} {\bf Low}
---   & Within 5 days
---   & Non-blocking changes, information requests
---   & 80\% \\ [0.2cm]
---   \hline
---
--- \end{tabular}
-
 
 local function generate_tabularray(tbl)
---  local table_class = 'tabular'
---
---print(tbl.attributes)
---  if (tbl.attributes['tablename'] ~= nil) then
---    table_class = tbl.attributes['tablename']
---  end
 
 local caption = pandoc.utils.stringify(tbl.caption.long)
 local caption_content = caption:match("{(.-)}")
@@ -105,16 +62,11 @@ end
 
   -- COLSPECS
   local col_specs = tbl.colspecs
-  --print(col_specs)
   local col_specs_latex = '| '
 
   for i, col_spec in ipairs(col_specs) do
     local align = col_spec[1]
     local width = col_spec[2]
-
---    if width ~= 0 and width ~= nil then
---      col_specs_latex = col_specs_latex .. width..'\\linewidth,'
---    end
 
     if align == 'AlignLeft' then
       col_specs_latex = col_specs_latex .. 'l |'
@@ -132,23 +84,11 @@ end
    local dict = {}
    for key, value in string.gmatch(caption_content, '(%w+)=([^%s]+)') do
        dict[key] = value
-      --print(key)
-      --print(value)
    end
 
-    --print(dict)
-
---   if dict["tablename"] then
---     table_class = dict["tablename"]
---   end
---   if dict["colspec"] then
---     col_specs_latex = dict["colspec"]
---   end
  end
 
   local result = pandoc.List:new{pandoc.RawBlock("latex", '\\begin{tabular}{ '.. col_specs_latex .. ' } \n \\hline')}
-
-  --local result = pandoc.List:new{pandoc.RawBlock("latex", '\\begin{tabular}{| >{\\raggedleft\\arraybackslash}p{1.7cm} | >{\\raggedright\\arraybackslash}p{4.7cm} | >{\\raggedright\arraybackslash}p{2.7cm}  | >{\\raggedright\arraybackslash}p{2.7cm}}')}
 
   -- HEADER
   local header_latex = get_rows_data(tbl.head.rows, 'tableheadercolor', false)
@@ -177,4 +117,3 @@ if FORMAT:match 'latex' then
   end
 
 end
-
